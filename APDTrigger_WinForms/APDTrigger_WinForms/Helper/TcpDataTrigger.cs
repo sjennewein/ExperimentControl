967935bcb9a9354bbc661ae21dcf459bc5b7f41f
Stephan Jennewein <stephan@selfnet.de>
2013-09-28 10:45:30 +0200

    fixed network communication

--- APDTrigger_WinForms/APDTrigger_WinForms/Helper/TcpDataTrigger.cs
+++ APDTrigger_WinForms/APDTrigger_WinForms/Helper/TcpDataTrigger.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 
 namespace APDTrigger_WinForms.Helper
 {
     public class TcpDataTrigger
     {
-        private readonly AutoResetEvent _myTriggerGate = new AutoResetEvent(false);
         private readonly AutoResetEvent _myClientGate = new AutoResetEvent(false);
+        private readonly AutoResetEvent _myTriggerGate = new AutoResetEvent(false);
 
         public NetworkData Data;
 
@@ -24,20 +22,19 @@ namespace APDTrigger_WinForms.Helper
         private void StartUdp()
         {
             var listener = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            UdpClient test = new UdpClient(9898);
-            
+            var test = new UdpClient(9898);
         }
 
 
         private void StartTcp()
         {
-            var listener = new TcpListener(IPAddress.Any, 9898);
+            var listener = new TcpListener(IPAddress.Any, 51111);
 
             listener.Start();
             while (true)
             {
                 IAsyncResult result = listener.BeginAcceptTcpClient(HandleAsyncConnection, listener);
-                _myClientGate.WaitOne();                
+                _myClientGate.WaitOne();
             }
         }
 
@@ -48,111 +45,66 @@ namespace APDTrigger_WinForms.Helper
 
         private void HandleAsyncConnection(IAsyncResult result)
         {
-            
-            TcpListener listener = (TcpListener) result.AsyncState;
+            var listener = (TcpListener) result.AsyncState;
             TcpClient client = listener.EndAcceptTcpClient(result);
             _myClientGate.Set();
-            
-            
-            client.NoDelay = true;
-            client.Client.NoDelay = true;            
-            
-            try
-            {         
 
+
+            client.NoDelay = true;
+            client.Client.NoDelay = true;
+
+            try
+            {
                 // get the stream to talk to the client over
-                using (NetworkStream ns = client.GetStream())    
+                using (NetworkStream ns = client.GetStream())
                 {
                     // set initial read timeout to 1 minute to allow for connection
                     ns.ReadTimeout = 60000;
                     ns.WriteTimeout = 1000;
-
+                    var w = new BinaryWriter(ns);
+                    var r = new BinaryReader(ns);
                     // handshaking
-                    SendMessage(ns, "HELLO?");
+                    w.Write("HELLO?");
+                    w.Flush();
                     Console.WriteLine("trying to shake hands");
-                    var test = ReceiveMessage(ns).Trim('\0');
+                    string test = r.ReadString();
                     if (test != "HELLO!")
                         throw new Exception("Wrong handshake message!");
                     Console.WriteLine("handshake successful");
                     // decrease read timeout to 1 second now that data is coming in
                     ns.ReadTimeout = 1000;
-                    
-                    SendMessage(ns, "DATA/TRIGGER?");
-                    
-                    switch(ReceiveMessage(ns))
+
+
+                    w.Write("DATA/TRIGGER?");
+                    w.Flush();
+
+                    switch (r.ReadString())
                     {
                         case "DATA!":
-                            var message = "NODATA!";
-                            
-                            if(Data != null)
+                            string message = "NODATA!";
+
+                            if (Data != null)
                             {
-                                message = Data.Serialize();    
+                                message = Data.Serialize();
                             }
-                            
-                            SendMessage(ns, message);
+
+                            w.Write(message);
+                            w.Flush();
                             break;
-                        case "TRIGGER!":                            
+                        case "TRIGGER!":
                             _myTriggerGate.WaitOne();
-                            SendMessage(ns, "GO!");
+                            w.Write("GO!");
                             break;
                     }
-                                       
+
                     ns.Close();
-
                 }
-
-            }
-            //catch (Exception)
-            //{
-
-            //}
+            }            
             finally
-            {                
+            {
                 if (client != null)
                     client.Close();
-                
             }
-            
         }
-
-        private string ReceiveMessage(NetworkStream ns)
-        {
-            byte[] bytes = new byte[4096];
-            var encoder = new ASCIIEncoding();
-            var clientData = new StringBuilder();
-            int bytesRead = 0;
-            string message = "";
-            
-            // Loop to receive all the data sent by the client.
-            do
-            {
-            //    try
-            //    {
-                    bytesRead = ns.Read(bytes, 0, bytes.Length);
-                    if (bytesRead > 0)
-                    {
-                        // Translate data bytes to an ASCII string
-                        clientData.Append(encoder.GetString(bytes, 0, bytesRead));
-
-                    }
-                //}
-                //catch (IOException ioe)
-                //{
-                   // bytesRead = 0;
-                //}
-            } while (bytesRead > 0);
-            
-            message = clientData.ToString();
-            return message;
-        }
-
-        private void SendMessage(NetworkStream ns, string message)
-        {
-            var encoder = new ASCIIEncoding();
-            byte[] bytes = encoder.GetBytes(message);                    
-            ns.Write(bytes, 0, bytes.Length);
-            ns.Flush();
-        }
-        
     }
 }

6bd32279d4aadfcfa15a61591f8d6156de014e52
Stephan Jennewein <stephan@selfnet.de>
2013-10-01 11:39:57 +0200

    network working

--- APDTrigger_WinForms/APDTrigger_WinForms/Helper/TcpDataTrigger.cs
+++ APDTrigger_WinForms/APDTrigger_WinForms/Helper/TcpDataTrigger.cs
@@ -2,18 +2,17 @@
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 
 namespace APDTrigger_WinForms.Helper
 {
     public class TcpDataTrigger
     {
+        private readonly TcpListener _listener;
         private readonly AutoResetEvent _myClientGate = new AutoResetEvent(false);
         private readonly AutoResetEvent _myTriggerGate = new AutoResetEvent(false);
-        private readonly TcpListener _listener;
-        private bool _run = true;
         public NetworkData Data;
+        private bool _run = true;
 
         public TcpDataTrigger()
         {
@@ -24,14 +23,12 @@ namespace APDTrigger_WinForms.Helper
 
         private void StartTcp()
         {
-            
-
             _listener.Start();
             while (_run)
             {
                 try
                 {
-                    IAsyncResult result = _listener.BeginAcceptTcpClient(HandleAsyncConnection, _listener);    
+                    IAsyncResult result = _listener.BeginAcceptTcpClient(HandleAsyncConnection, _listener);
                 }
                 catch
                 {
@@ -48,8 +45,6 @@ namespace APDTrigger_WinForms.Helper
         {
             _run = false;
             _myClientGate.Set();
-            
-            
         }
 
         public void Trigger()
@@ -63,21 +58,21 @@ namespace APDTrigger_WinForms.Helper
             TcpClient client;
             try
             {
-                 client = listener.EndAcceptTcpClient(result);
+                client = listener.EndAcceptTcpClient(result);
             }
             catch (Exception)
-            {                
+            {
                 return;
             }
             finally
             {
                 _myClientGate.Set();
             }
-            
+
 
             client.NoDelay = true;
             client.Client.NoDelay = true;
-            Data = new NetworkData(0, 2, 23, 921, 23, 2.1);
+            //Data = new NetworkData(0, 2, 23, 921, 23, 2.1);
             try
             {
                 // get the stream to talk to the client over
@@ -87,23 +82,22 @@ namespace APDTrigger_WinForms.Helper
                     ns.ReadTimeout = 1000;
                     ns.WriteTimeout = 1000;
                     var w = new BinaryWriter(ns);
-                    //var r = new BinaryReader(ns);
+                    var r = new BinaryReader(ns);
                     // handshaking
                     w.Write("HELLO!");
                     w.Flush();
                     Console.WriteLine("trying to shake hands");
 
-                    
-                    if (ReceiveMessage(ns) != "HELLO!")
+                    // var test = ReceiveMessage(ns);
+                    string test = r.ReadString();
+                    if (test != "HELLO!")
                         throw new Exception("Wrong handshake message!");
                     Console.WriteLine("handshake successful");
-                    //// decrease read timeout to 1 second now that data is coming in
-                    //ns.ReadTimeout = 1000;
 
                     w.Write("DATA/TRIGGER!");
                     w.Flush();
 
-                    switch (ReceiveMessage(ns))
+                    switch (r.ReadString())
                     {
                         case "DATA!":
                             string message = "NODATA";
@@ -121,83 +115,25 @@ namespace APDTrigger_WinForms.Helper
                             break;
                     }
 
-                    
 
-                    if (ReceiveMessage(ns) == "BYE!")
+                    if (r.ReadString() == "BYE!")
                     {
-
                         w.Write("BYE!");
                         w.Flush();
                     }
 
-                   
 
                     ns.Close();
                 }
             }
             catch
             {
-                 
             }
             finally
             {
                 if (client != null)
                     client.Close();
             }
-        }
-
-        private string ReceiveMessage(NetworkStream ns)
-        {
-            byte[] bytes = new byte[1024];
-            var encoder = new ASCIIEncoding();
-            var clientData = new StringBuilder();
-            int bytesRead = 0;
-            string message = "";            
-            bool run = true;
-            int loopCounter = 0;
-
-            // Loop to receive all the data sent by the client.
-            do
-            {
-                //check if data is available
-                if (ns.DataAvailable)
-                {
-                    //set how much data has been read
-                    bytesRead = ns.Read(bytes, 0, bytes.Length);
-                }
-                else
-                {
-                    //if no data was available it's 0
-                    bytesRead = 0;
-                }
-
-                if (bytesRead > 0)
-                {
-                    // Translate data bytes to an ASCII string and append
-                    clientData.Append(encoder.GetString(bytes, 0, bytesRead));
-                }
-                
-
-                message = clientData.ToString();
-                
-                if(message.Length > 0)
-                {
-                    //check if termination byte appeared
-                    if (message[message.Length - 1].ToString() == "!")
-                        //if yes stop receiving data
-                        run = false;
-                }
-
-                if (loopCounter > 10)
-                {
-                    throw new Exception("Broken communication :(");
-                }
-                    
-
-                loopCounter++;
-            } while (run);
-            
-            return message;
         }
     }
 }

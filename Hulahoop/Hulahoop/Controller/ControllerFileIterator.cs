b50133800d909c7a503be13cd51baca313eabbea
stephan jennewein <stephan@selfnet.de>
2014-06-02 09:51:48 +0200

    iterator now shows iterations after loading

--- Hulahoop/Hulahoop/Controller/ControllerFileIterator.cs
+++ Hulahoop/Hulahoop/Controller/ControllerFileIterator.cs
@@ -19,6 +19,8 @@ namespace Hulahoop.Controller
         public ControllerFileIterator(ModelFileIterator model)
         {
             _model = model;
+            if(_model.Iterations != null)
+                _fileLength = _model.Iterations.Length;
         }
 
         public string Name
